1748396df6287cb2ce8f3791318e71170a878040
Thiago Oliveira <pintowar@gmail.com>
2021-10-29 14:15:19 -0300

    adding client assets to native image

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     implementation("org.jetbrains.kotlin:kotlin-allopen:1.5.31")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.5.30")
-    implementation("io.micronaut.gradle:micronaut-gradle-plugin:2.0.6")
+    implementation("io.micronaut.gradle:micronaut-gradle-plugin:2.0.8")
     implementation("org.bytedeco.gradle-javacpp-platform:org.bytedeco.gradle-javacpp-platform.gradle.plugin:1.5.6")
     implementation("gradle.plugin.com.github.jengelman.gradle.plugins:shadow:7.0.0")
     implementation("com.github.node-gradle:gradle-node-plugin:3.1.1")
