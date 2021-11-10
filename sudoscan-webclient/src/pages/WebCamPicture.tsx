84118b11116b98b5850d75bdf5c65f4a0bf15247
Thiago Oliveira <pintowar@gmail.com>
2021-11-10 17:54:55 -0300

    changing sudoscan picture interface

--- sudoscan-webclient/src/pages/WebCamPicture.tsx
+++ sudoscan-webclient/src/pages/WebCamPicture.tsx
@@ -1,5 +1,5 @@
-import { useRef, useState } from "react";
-import { FaCamera, FaRegTrashAlt } from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import { FaPen, FaCamera, FaTrashAlt, FaImages, FaArrowAltCircleRight } from 'react-icons/fa';
 import Webcam from "react-webcam";
 import axios from 'axios';
 
@@ -12,23 +12,43 @@ export const WebCamPicture = () => {
     const [solutionColor, setSolutionColor] = useState('BLUE');
     const [recognizerColor, setRecognizerColor] = useState('NONE');
     const [processing, setProcessing] = useState(false);
+    const [sampleImages, setSampleImages] = useState<string[]>([]);
     
     const [alert, setAlert] = useState("");
-    const webcamRef = useRef<Webcam>(null);
+    
     const [imgWidth, imgHeight] = [480, 360]
-    const videoConstraints = {
-        width: imgWidth,
-        height: imgHeight,
-        facingMode: "environment"
-      };
+    
     const imgStyle = {width: `${imgWidth}px`, height: `${imgHeight}px`}
 
-    const capture = async () => {
+    const base64Image = async (url: string) => {
+        const data = await fetch(url);
+        const blob = await data.blob();
+        return new Promise<string>((resolve) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(blob); 
+            reader.onloadend = () => {
+                const base64data = reader.result;   
+                resolve(`${base64data}`);
+            }
+        });
+    }
+
+    useEffect(() => {
+        const initSample  = ["./sudoku01.jpg", "./sudoku02.jpg"];
+        Promise.all(initSample.map(base64Image)).then(img =>
+            setSampleImages(sampleImages.concat(img))
+        )
+    }, []);
+
+    const clean = () => {
+        setImgSource(noImage)
+    }
+
+    const solve = async () => {
         try {
             setProcessing(true)
-            const screenshot = webcamRef.current?.getScreenshot() || ""
-            if(screenshot !== "") {
-                const res = await axios.post('/api/solve', { encodedImage: screenshot, solutionColor, recognizerColor })
+            if(imgSource !== noImage) {
+                const res = await axios.post('/api/solve', { encodedImage: imgSource, solutionColor, recognizerColor })
                 setImgSource(res.data)
             }
         } catch ({ response: {data, status} }) {
@@ -38,47 +58,86 @@ export const WebCamPicture = () => {
         }
     }
 
-    const clean = () => {
-        setImgSource(noImage)
+    const removeSample = (idx: number) => {
+        const copy = [...sampleImages];
+        copy.splice(idx, 1);
+        setSampleImages(copy);
     }
 
     return (
-        <div className="container pt-16 mx-auto items-center">
-            { alert && <AlertMessage message={alert} setMessage={setAlert} /> }
+        <div className="flex flex-1 h-full">
 
-            <EngineInfoLabel/>
+            <aside id="sidebar" className="bg-side-nav w-1/2 md:w-1/6 lg:w-1/6 border-r border-side-nav hidden md:block lg:block py-16" style={{ display: 'block' }}>
 
-            <div className="flex flex-wrap justify-center space-x-5 pt-4">
-                <Webcam audio={false} ref={webcamRef} videoConstraints={videoConstraints} />
-                <img src={imgSource} className="object-scale-down" style={imgStyle} alt="webcam-capture"/>
-            </div>
+                <ul className="list-reset flex flex-col">
+                    <li className=" w-full h-full py-3 px-2 border-b border-light-border bg-white flex relative">
+                        <FaImages className="h-6 w-6"/>
+                        <span className="ml-2">Pictures</span>
+
+                        <button onClick={clean} className="inline-block px-1 mr-2 absolute right-0 shadow ripple hover:shadow-lg bg-gray-600 hover:bg-gray-800 rounded-full text-center text-white font-bold waves-effect">
+                            <span >+</span>
+                        </button>
+                    </li>
+                    {sampleImages.map((src, idx) => 
+                        <li key={idx} className="my-1 border-2 relative">
+                            <button onClick={() => removeSample(idx)} className="px-2 py-1 my-1 absolute right-0 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex">
+                                <FaTrashAlt className="h-3 w-3"/>
+                            </button>
+                            <button onClick={() => setImgSource(src)} className="px-2 py-1 my-8 absolute right-0 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex">
+                                <FaArrowAltCircleRight className="h-3 w-3"/>
+                            </button>
+                            <img src={src} alt="Dbl Click" className="w-full h-48" />
+                        </li>
+                    )
+                    }
+                    
+                </ul>
+
+            </aside>
+
+            <main className="bg-white-300 flex-1 p-3 overflow-hidden py-16">
+
+                <div className="flex flex-col">
+
+                    { alert && <AlertMessage message={alert} setMessage={setAlert} /> }
+
+                    <EngineInfoLabel/>
+
+                    <div className="flex flex-wrap justify-center space-x-5 pt-4">
+                        <img src={imgSource} className="object-scale-down" style={imgStyle} alt="webcam-capture"/>
+                    </div>
+
+                    <div className="flex flex-wrap justify-center py-5 space-x-5">
+                    <select value={solutionColor} onChange={(e) => setSolutionColor(e.target.value)} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex" >
+                            <option value="NONE" className="text-black bg-white">None</option>
+                            <option value="BLUE" className="text-blue-500 bg-white">Blue</option>
+                            <option value="GREEN" className="text-green-500 bg-white">Green</option>
+                            <option value="RED" className="text-red-500 bg-white">Red</option>
+                        </select>
+
+                        <select value={recognizerColor} onChange={(e) => setRecognizerColor(e.target.value)} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex" >
+                            <option value="NONE" className="text-black bg-white">None</option>
+                            <option value="BLUE" className="text-blue-500 bg-white">Blue</option>
+                            <option value="GREEN" className="text-green-500 bg-white">Green</option>
+                            <option value="RED" className="text-red-500 bg-white">Red</option>
+                        </select>
+
+                        <button disabled={processing} onClick={solve} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex">
+                            <FaPen className="h-6 w-6"/>
+                            <span className="ml-2">Solve</span>
+                        </button>
+
+                        <button disabled={processing} onClick={clean} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex">
+                            <FaTrashAlt className="h-6 w-6"/>
+                            <span className="ml-2">Clean</span>
+                        </button>
+                        {processing && <div className="px-4 py-3 my-5 w-12 h-12 border-4 border-gray-600 rounded-full loader" />}
+
+                    </div> 
+                    
+                </div>
+            </main>
             
-            <div className="flex flex-wrap justify-center py-5 space-x-5">
-            <select value={solutionColor} onChange={(e) => setSolutionColor(e.target.value)} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex" >
-                    <option value="NONE" className="text-black bg-white">None</option>
-                    <option value="BLUE" className="text-blue-500 bg-white">Blue</option>
-                    <option value="GREEN" className="text-green-500 bg-white">Green</option>
-                    <option value="RED" className="text-red-500 bg-white">Red</option>
-                </select>
-
-                <select value={recognizerColor} onChange={(e) => setRecognizerColor(e.target.value)} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex" >
-                    <option value="NONE" className="text-black bg-white">None</option>
-                    <option value="BLUE" className="text-blue-500 bg-white">Blue</option>
-                    <option value="GREEN" className="text-green-500 bg-white">Green</option>
-                    <option value="RED" className="text-red-500 bg-white">Red</option>
-                </select>
-
-                <button disabled={processing} onClick={capture} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex">
-                    <FaCamera className="h-6 w-6"/>
-                    <span className="ml-2">Capture</span>
-                </button>
-
-                <button disabled={processing} onClick={clean} className="px-4 py-3 my-5 bg-gray-600 hover:bg-gray-800 rounded-full text-white font-bold flex">
-                    <FaRegTrashAlt className="h-6 w-6"/>
-                    <span className="ml-2">Clean</span>
-                </button>
-                {processing && <div className="px-4 py-3 my-5 w-12 h-12 border-4 border-gray-600 rounded-full loader" />}
-            </div>
         </div>
     );
 };
